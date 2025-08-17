7f08b182c928182a0524125a0bbeae5791765437
0hj1hyeon <128037333+0hj1hyeon@users.noreply.github.com>
2025-08-17 11:31:32 +0900

    [Bronze V] Title: 새싹, Time: 160 ms, Memory: 17584 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        
+            System.out.println("         ,r'\"7\n" +
+                    "r`-_   ,'  ,/\n" +
+                    " \\. \". L_r'\n" +
+                    "   `~\\/\n" +
+                    "      |\n" +
+                    "      |");
+       
+    }
+}
