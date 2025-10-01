3abd255343d5f5d153c054199a47c5f81f564a61
0hj1hyeon <128037333+0hj1hyeon@users.noreply.github.com>
2025-10-01 11:34:04 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 120 ms, Memory: 15936 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] dp = new int[1001];
+
+
+        // 초기값 설정
+        dp[1] = 1;
+        dp[2] = 3;
+
+        // 점화식을 이용한 계산
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i - 1] + 2 * dp[i - 2]) % 10007;
+        }
+
+        bw.write(dp[n] + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
